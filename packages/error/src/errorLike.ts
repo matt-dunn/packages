149f3e6e6548f33e7b370a547cab6753437eca80
Matt Dunn <matt.j.dunn@gmail.com>
2020-08-29 15:24:14 +0100

    refactor(error): rework index

---
+++ packages/error/src/errorLike.ts
@@ -0,0 +1,15 @@
+import { ErrorLike } from "./types";
+
+export const errorLike = (error: ErrorLike & {[index: string]: any}): ErrorLike => {
+  const { stack, ...rest } = Object.getOwnPropertyNames(error).reduce((o: any, key: string) => { // eslint-disable-line @typescript-eslint/no-unused-vars
+    o[key] = error[key];
+    return o;
+  }, {});
+
+  return rest;
+};
+
+export function isErrorLike (o: any): o is ErrorLike {
+  return o && o.message && o.name;
+}
+
